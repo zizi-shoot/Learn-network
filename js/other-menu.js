1b956b42ff306ab1059cddf25c0184318adee1e3
zizi-shoot <ol.polyanskiy@gmail.com>
2020-06-29 01:29:14 +0300

    add script for other-menu

---
+++ js/other-menu.js
@@ -0,0 +1,26 @@
+'use strict'
+
+let otherOpenBtn = document.querySelector('.header__other-btn');
+let otherCloseBtn = document.querySelector('.other-header__close-btn');
+let otherMenu = document.querySelector('.header__other')
+let blur = document.createElement('div');
+blur.className = 'blur';
+
+otherOpenBtn.addEventListener('click', displayOtherMenu);
+otherCloseBtn.addEventListener('click', hideOtherMenu);
+
+function displayOtherMenu() {
+	otherCloseBtn.style.top = '0';
+	otherMenu.style.top = '0';
+	setTimeout(() => {
+		document.body.append(blur);
+	}, 700);
+
+}
+function hideOtherMenu() {
+	otherCloseBtn.style.top = '-100vh';
+	otherMenu.style.top = '-100vh';
+	setTimeout(() => {
+		blur.remove();
+	}, 700);
+}
