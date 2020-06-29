29727dd08bff442bf2cd8dce50dd5ed7375f0bcb
zizi-shoot <ol.polyanskiy@gmail.com>
2020-06-29 12:31:56 +0300

    some fix for validator

--- js/upload-pic.js
+++ js/upload-pic.js
@@ -1,3 +1,12 @@
 'use strict';
 
+let uploadButton = document.querySelector('.inputfile');
+let imgPath;
+uploadButton.addEventListener('change', uploadPic);
+
+function uploadPic(event) {
+	imgPath = URL.createObjectURL(event.target.files[0]);
+	document.querySelector('.hero__pic').setAttribute('src', imgPath)
+}
+
 
