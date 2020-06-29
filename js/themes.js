574299117ede67463fe491fcbc80ba56c09d7e45
zizi-shoot <ol.polyanskiy@gmail.com>
2020-06-29 08:23:14 +0300

    some fix

--- js/themes.js
+++ js/themes.js
@@ -27,33 +27,33 @@ function toggleTheme() {
 	if (localStorage.getItem('theme') === 'light-theme') {
 		initialState('dark-theme');
 		modeSwitch.innerHTML = 'Light';
-		modeSwitch.style.backgroundImage = 'url(../img/light_mode.svg)';
-		linkFeed.style.backgroundImage = 'url(../img/head-feed_copy.svg)';
-		linkNetwork.style.backgroundImage = 'url(../img/head-network_copy.svg)';
-		linkJobs.style.backgroundImage = 'url(../img/head-jobs_copy.svg)';
-		linkChat.style.backgroundImage = 'url(../img/head-chat_copy.svg)';
-		linkNotices.style.backgroundImage = 'url(../img/head-notices_copy.svg)';
-		moreVertical.style.backgroundImage = 'url(../img/more-vertical_copy.svg)';
-		moreHorizontal.style.backgroundImage = 'url(../img/more-horizontal_copy.svg)';
-		closeBtn.style.backgroundImage = 'url(../img/x_copy.svg)';
-		uploadBtn.style.backgroundImage = 'url(../img/hero-upload_copy.svg)';
-		editBtn.style.backgroundImage = 'url(../img/hero-edit_copy.svg)';
+		modeSwitch.style.backgroundImage = 'url(img/light_mode.svg)';
+		linkFeed.style.backgroundImage = 'url(img/head-feed_copy.svg)';
+		linkNetwork.style.backgroundImage = 'url(img/head-network_copy.svg)';
+		linkJobs.style.backgroundImage = 'url(img/head-jobs_copy.svg)';
+		linkChat.style.backgroundImage = 'url(img/head-chat_copy.svg)';
+		linkNotices.style.backgroundImage = 'url(img/head-notices_copy.svg)';
+		moreVertical.style.backgroundImage = 'url(img/more-vertical_copy.svg)';
+		moreHorizontal.style.backgroundImage = 'url(img/more-horizontal_copy.svg)';
+		closeBtn.style.backgroundImage = 'url(img/x_copy.svg)';
+		uploadBtn.style.backgroundImage = 'url(img/hero-upload_copy.svg)';
+		editBtn.style.backgroundImage = 'url(img/hero-edit_copy.svg)';
 
 
 	} else {
 		initialState('light-theme');
 		modeSwitch.innerHTML = 'Dark';
-		modeSwitch.style.backgroundImage = 'url(../img/dark_mode.svg)';
-		linkFeed.style.backgroundImage = 'url(../img/head-feed.svg)';
-		linkNetwork.style.backgroundImage = 'url(../img/head-network.svg)';
-		linkJobs.style.backgroundImage = 'url(../img/head-jobs.svg)';
-		linkChat.style.backgroundImage = 'url(../img/head-chat.svg)';
-		linkNotices.style.backgroundImage = 'url(../img/head-notices.svg)';
-		moreVertical.style.backgroundImage = 'url(../img/more-vertical.svg)';
-		moreHorizontal.style.backgroundImage = 'url(../img/more-horizontal.svg)';
-		closeBtn.style.backgroundImage = 'url(../img/x.svg)';
-		uploadBtn.style.backgroundImage = 'url(../img/hero-upload.svg)';
-		editBtn.style.backgroundImage = 'url(../img/hero-edit.svg)';
+		modeSwitch.style.backgroundImage = 'url(img/dark_mode.svg)';
+		linkFeed.style.backgroundImage = 'url(img/head-feed.svg)';
+		linkNetwork.style.backgroundImage = 'url(img/head-network.svg)';
+		linkJobs.style.backgroundImage = 'url(img/head-jobs.svg)';
+		linkChat.style.backgroundImage = 'url(img/head-chat.svg)';
+		linkNotices.style.backgroundImage = 'url(img/head-notices.svg)';
+		moreVertical.style.backgroundImage = 'url(img/more-vertical.svg)';
+		moreHorizontal.style.backgroundImage = 'url(img/more-horizontal.svg)';
+		closeBtn.style.backgroundImage = 'url(img/x.svg)';
+		uploadBtn.style.backgroundImage = 'url(img/hero-upload.svg)';
+		editBtn.style.backgroundImage = 'url(img/hero-edit.svg)';
 	}
 }
 
